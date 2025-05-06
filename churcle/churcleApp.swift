9fead683740f3eb5b56659dd29691c05e4ee6ded
mertalada <141673372+mertalada@users.noreply.github.com>
2025-05-06 11:46:29 +0300

    Initial Commit

---
+++ churcle/churcleApp.swift
@@ -0,0 +1,17 @@
+//
+//  churcleApp.swift
+//  churcle
+//
+//  Created by Mert Aladağ on 6.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct churcleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ churcleTests/churcleTests.swift
@@ -0,0 +1,17 @@
+//
+//  churcleTests.swift
+//  churcleTests
+//
+//  Created by Mert Aladağ on 6.05.2025.
+//
+
+import Testing
+@testable import churcle
+
+struct churcleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
